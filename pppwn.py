33a80e663c845128d0d056c222f659775d0a78a8
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2024-05-02 20:26:09 +0100

    ✨ Added support for FW 8.50

--- pppwn.py
+++ pppwn.py
@@ -820,7 +820,7 @@ class Exploit():
 def main():
     parser = ArgumentParser('pppwn.py')
     parser.add_argument('--interface', required=True)
-    parser.add_argument('--fw', choices=['900', '903', '904', '950', '960', '1000', '1001', '1050','1070','1071', '1100'], default='1100')
+    parser.add_argument('--fw', choices=['850', '900', '903', '904', '950', '960', '1000', '1001', '1050','1070','1071', '1100'], default='1100')
     parser.add_argument('--stage1', default='stage1/stage1.bin')
     parser.add_argument('--stage2', default='stage2/stage2.bin')
     args = parser.parse_args()
@@ -834,7 +834,9 @@ def main():
     with open(args.stage2, mode='rb') as f:
         stage2 = f.read()
 
-    if args.fw == '900':
+    if args.fw == '850':
+        offs = OffsetsFirmware_850()
+    elif args.fw == '900':
         offs = OffsetsFirmware_900()
     elif args.fw in ('903', '904'):
         offs = OffsetsFirmware_903_904()
