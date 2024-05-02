631fb79deef9f715b7331d1292cd0bddc0700520
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-05-01 20:21:07 -0600

    hotfix

--- pppwn.py
+++ pppwn.py
@@ -837,7 +837,7 @@ def main():
     if args.fw == '900':
         offs = OffsetsFirmware_900()
     elif args.fw in ('1050', '1070', '1071'):
-        offs = OffsetsFirmware_1000()
+        offs = OffsetsFirmware_1050()
     elif args.fw == '1100':
         offs = OffsetsFirmware_1100()
 
