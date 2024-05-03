0ba072801e42864cf40e937c8255ebfadbe70b51
kiwidog <forums@allenthinks.com>
2024-05-03 13:47:46 -0700

    Add offsets to offsets.py Add 672 suppport to pppwn, pending 4 missing ROP gadgets

--- offsets.py
+++ offsets.py
@@ -3,6 +3,99 @@
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
 
+# FW 6.72
+class OffsetsFirmware_672:
+    PPPOE_SOFTC_LIST = 0xFFFFFFFF844BCCB0
+
+    KERNEL_MAP = 0xFFFFFFFF8440DFC0
+
+    SETIDT = 0xFFFFFFFF822A1B90
+
+    KMEM_ALLOC = 0xFFFFFFFF82450730
+    KMEM_ALLOC_PATCH1 = 0xFFFFFFFF824507F5
+    KMEM_ALLOC_PATCH2 = 0xFFFFFFFF82450803
+
+    MEMCPY = 0xFFFFFFFF825C15B0
+
+    # 0xffffffff823fb949 : mov cr0, rsi ; ud2 ; mov eax, 1 ; ret
+    MOV_CR0_RSI_UD2_MOV_EAX_1_RET = 0xFFFFFFFF82368769
+
+    SECOND_GADGET_OFF = 0x3d
+
+    # 0xffffffff82996603 : jmp qword ptr [rsi + 0x3d]
+    FIRST_GADGET = 0xffffffff82996603 # NEED
+
+    # 0xffffffff82c76646 : push rbp ; jmp qword ptr [rsi]
+    PUSH_RBP_JMP_QWORD_PTR_RSI = 0xffffffff82c960f6
+
+    # 0xffffffff822b4151 : pop rbx ; pop r14 ; pop rbp ; jmp qword ptr [rsi + 0x10]
+    POP_RBX_POP_R14_POP_RBP_JMP_QWORD_PTR_RSI_10 = 0xffffffff8232253f
+
+    # 0xffffffff82941e46 : lea rsp, [rsi + 0x20] ; repz ret
+    LEA_RSP_RSI_20_REPZ_RET = 0xffffffff8295bac6
+
+    # 0xffffffff826c52aa : add rsp, 0x28 ; pop rbp ; ret
+    ADD_RSP_28_POP_RBP_RET = 0xffffffff826c52aa # NEED
+
+    # 0xffffffff8251b08f : add rsp, 0xb0 ; pop rbp ; ret
+    ADD_RSP_B0_POP_RBP_RET = 0xffffffff82518d2b
+
+    # 0xffffffff822008e0 : ret
+    RET = 0xffffffff822008f3
+
+    # 0xffffffff822391a8 : pop rdi ; ret
+    POP_RDI_RET = 0xffffffff8225a5f5
+
+    # 0xffffffff822aad39 : pop rsi ; ret
+    POP_RSI_RET = 0xffffffff8223593e
+
+    # 0xffffffff82322eba : pop rdx ; ret
+    POP_RDX_RET = 0xffffffff82367852
+
+    # 0xffffffff822445e7 : pop rcx ; ret
+    POP_RCX_RET = 0xffffffff82233adf
+
+    # 0xffffffff822ab4dd : pop r8 ; pop rbp ; ret
+    POP_R8_POP_RBP_RET = 0xffffffff822ab4dd # NEED
+
+    # 0xffffffff8279fa0f : pop r12 ; ret
+    POP_R12_RET = 0xffffffff825bd011
+
+    # 0xffffffff82234ec8 : pop rax ; ret
+    POP_RAX_RET = 0xffffffff822ba2d6
+
+    # 0xffffffff822008df : pop rbp ; ret
+    POP_RBP_RET = 0xffffffff822008f2
+
+    # 0xffffffff82bb687a : push rsp ; pop rsi ; ret
+    PUSH_RSP_POP_RSI_RET = 0xffffffff82bd4e1a
+
+    # 0xffffffff82244ed0 : mov rdi, qword ptr [rdi] ; pop rbp ; jmp rax
+    MOV_RDI_QWORD_PTR_RDI_POP_RBP_JMP_RAX = 0xffffffff82244ed0 # NEED
+
+    # 0xffffffff82b7450e : mov byte ptr [rcx], al ; ret
+    MOV_BYTE_PTR_RCX_AL_RET = 0xffffffff82b92a7a
+
+    # 0xffffffff82632b9c : mov rdi, rbx ; call r12
+    MOV_RDI_RBX_CALL_R12 = 0xffffffff824c9359
+
+    # 0xffffffff8235b387 : mov rdi, r14 ; call r12
+    MOV_RDI_R14_CALL_R12 = 0xffffffff822e3627
+
+    # 0xffffffff822e3d7e : mov rsi, rbx ; call rax
+    MOV_RSI_RBX_CALL_RAX = 0xffffffff82415ce1
+
+    # 0xffffffff82363918 : mov r14, rax ; call r8
+    MOV_R14_RAX_CALL_R8 = 0xffffffff824549a8
+
+    # 0xffffffff82cb683a : add rdi, rcx ; ret
+    ADD_RDI_RCX_RET = 0xffffffff82cd62ea
+
+    # 0xffffffff82409557 : sub rsi, rdx ; mov rax, rsi ; pop rbp ; ret
+    SUB_RSI_RDX_MOV_RAX_RSI_POP_RBP_RET = 0xffffffff82409557 # NEED
+
+    # 0xffffffff82b85693 : jmp r14
+    JMP_R14 = 0xffffffff82ba3c2b
 
 # FW 9.00
 class OffsetsFirmware_900:
